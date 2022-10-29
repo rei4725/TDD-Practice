e87bd7b44cc51c4583643647cae5ae8c608d8ee6
rei4725 <fuchikun@gmail.com>
2022-10-29 21:43:27 +0900

    テスト正常化

---
+++ TDDByExample/MultiCurrencyMoney/Franc.cs
@@ -0,0 +1,22 @@
+﻿namespace MultiCurrencyMoney
+{
+    public class Franc
+    {
+        private readonly int Amount;
+        public Franc(int amount)
+        {
+            Amount = amount;
+        }
+
+        public Franc Times (int timesValue)
+        {
+            return new Franc(Amount * timesValue);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var thatFranc = obj as Franc;
+            return Amount.Equals(thatFranc?.Amount);
+        }
+    }
+}
