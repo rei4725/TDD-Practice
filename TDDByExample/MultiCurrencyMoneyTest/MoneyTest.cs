e083bc0f03e292b979e62dd0ea50f1e67562de5a
rei4725 <fuchikun@gmail.com>
2022-10-29 21:02:19 +0900

    テストコード作成 [5$ * 2 = 10$]

---
+++ TDDByExample/MultiCurrencyMoneyTest/MoneyTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MultiCurrencyMoneyTest
+{
+    [TestClass]
+    public class MoneyTest
+    {
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            Dollor five = new Dollar(5);
+            five.Times(2);
+            Assert.AreEqual(10);
+        }
+    }
+}
