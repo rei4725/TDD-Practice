9379fe1f4e85bb1c99390eb33e82c99e189ce61f
rei4725 <fuchikun@gmail.com>
2022-10-29 21:31:21 +0900

    テスト正常化

--- TDDByExample/MultiCurrencyMoney/Dollor.cs
+++ TDDByExample/MultiCurrencyMoney/Dollor.cs
@@ -12,5 +12,11 @@
         {
             return new Dollar(Amount * timesValue);
         }
+
+        public override bool Equals(object obj)
+        {
+            var thatDollar = obj as Dollar;
+            return Amount.Equals(thatDollar?.Amount);
+        }
     }
 }
